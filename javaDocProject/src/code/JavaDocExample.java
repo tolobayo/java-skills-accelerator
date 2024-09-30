5c910a972bb62f39b1c147cdeb62a18debded8e7
tolobayo <tolobayo1@gmail.com>
2024-09-30 03:41:13 -0400

    inital commit with first two sampler projects

---
+++ javaDocProject/src/code/JavaDocExample.java
@@ -0,0 +1,17 @@
+package code;
+
+/** This is my first Java Doc
+ * @author Tolu
+ * 
+ */
+public class JavaDocExample {
+
+    /** This method returns a greeting
+     * 
+     * @return string - returns "hello" string
+     */
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
---
+++ myFirstProject/src/code/FirstClass.java
@@ -0,0 +1,9 @@
+package code;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("My first Java application");
+    }
+
+}
